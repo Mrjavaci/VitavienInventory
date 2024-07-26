9de36ccc641ba736098aa2ad71ebf4c51ffdec36
Osman Taha Kıyıcılar <tr.osmantaha@gmail.com>
2024-07-26 23:37:38 +0300

    cleaned System Module

--- Modules/System/App/Providers/SystemServiceProvider.php
+++ Modules/System/App/Providers/SystemServiceProvider.php
@@ -20,8 +20,6 @@ class SystemServiceProvider extends ServiceProvider
         $this->registerCommandSchedules();
         $this->registerTranslations();
         $this->registerConfig();
-        $this->registerViews();
-        $this->loadMigrationsFrom(module_path($this->moduleName, 'Database/migrations'));
     }
 
     /**
@@ -29,7 +27,6 @@ class SystemServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->register(RouteServiceProvider::class);
     }
 
     /**
--- Modules/System/resources/views/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('system::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('system.name') !!}</p>
-@endsection
