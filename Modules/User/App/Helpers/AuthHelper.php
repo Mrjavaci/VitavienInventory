7ce84b63519e5c4d99a4c6f2fb745f082825a536
Osman Taha Kıyıcılar <tr.osmantaha@gmail.com>
2024-07-28 12:21:53 +0300

    User type etc..

---
+++ Modules/User/App/Helpers/AuthHelper.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\User\App\Helpers;
+
+use Illuminate\Support\Facades\Auth;
+
+class AuthHelper
+{
+    public static function make(): self
+    {
+        return app()->make(self::class);
+    }
+
+    public function isLogged(): bool
+    {
+        return Auth::id() !== null;
+    }
+
+    public function getUser(): ?\Illuminate\Contracts\Auth\Authenticatable
+    {
+        return Auth::user();
+    }
+
+    public function getUserType()
+    {
+        return Auth::user()->user_type;
+    }
+
+}
--- Modules/User/Database/Seeders/UserDatabaseSeeder.php
+++ Modules/User/Database/Seeders/UserDatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Modules\User\Database\Seeders;
 
+use Faker\Factory;
 use Illuminate\Database\Seeder;
+use Modules\User\App\Models\User;
 
 class UserDatabaseSeeder extends Seeder
 {
@@ -11,6 +13,28 @@ class UserDatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $fakerFactory = Factory::create('tr_TR');
+        User::query()->create([
+            'name'      => $fakerFactory->name,
+            'email'     => $fakerFactory->email,
+            'password'  => $fakerFactory->password,
+            'user_type' => 'Branch',
+            'user_type_id' => 1
+        ]);
 
+        User::query()->create([
+            'name'      => $fakerFactory->name,
+            'email'     => $fakerFactory->email,
+            'password'  => $fakerFactory->password,
+            'user_type' => 'WareHouse',
+            'user_type_id' => 1
+        ]);
+        User::query()->create([
+            'name'      => $fakerFactory->name,
+            'email'     => $fakerFactory->email,
+            'password'  => $fakerFactory->password,
+            'user_type' => 'System',
+            'user_type_id' => 0
+        ]);
     }
 }
